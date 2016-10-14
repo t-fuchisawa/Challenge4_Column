f6d26069b7f5cb382fe97be58dd8d5d43a963c34
t-fuchisawa <fuchisawa@sancom.co.jp>
2016-10-14 11:49:00 +0900

    プロジェクト ファイルを追加します。

---
+++ Challenge4_Column/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Challenge4_Column
+{
+    public class Program
+    {
+        /// <summary>
+        /// メイン
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            if (args.Length >= 1)
+            {
+                int number;
+                if (int.TryParse(args[0], out number))
+                    Console.WriteLine(ConvertToColumnName(number));
+                else
+                    Console.WriteLine(ConvertToColumnNumber(args[0].ToUpper()));
+            }
+        }
+
+
+        /// <summary>
+        /// 数値をExcelの列名(ローマ字)に変換
+        /// </summary>
+        /// <param name="columnNumber"></param>
+        /// <returns></returns>
+        public static string ConvertToColumnName(int columnNumber)
+        {
+            string str = "";
+            int quot = columnNumber - 1;
+            while (quot >= 26)
+            {
+                int remain = quot % 26;
+                str = (char)('A' + remain) + str;
+                quot = quot / 26 - 1;
+            }
+            str = (char)('A' + quot) + str;
+            return str;
+        }
+
+
+        /// <summary>
+        /// Excelの列名(ローマ字)を数値に変換
+        /// </summary>
+        /// <param name="columnName"></param>
+        /// <returns></returns>
+        public static int ConvertToColumnNumber(string columnName)
+        {
+            int num = 0;
+            foreach (var c in columnName)
+            {
+                num *= 26;
+                num += (c - 'A') + 1;
+            }
+            return num;
+        }
+
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Challenge4_Column;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(Program.ConvertToColumnName(1), "A");
+            Assert.AreEqual(Program.ConvertToColumnName(3), "C");
+            Assert.AreEqual(Program.ConvertToColumnName(27), "AA");
+            Assert.AreEqual(Program.ConvertToColumnName(123), "DS");
+            Assert.AreEqual(Program.ConvertToColumnName(2730), "CZZ");
+            Assert.AreEqual(Program.ConvertToColumnName(2731), "DAA");
+            Assert.AreEqual(Program.ConvertToColumnName(2147483647), "FXSHRXW");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(Program.ConvertToColumnNumber("A"), 1);
+            Assert.AreEqual(Program.ConvertToColumnNumber("C"), 3);
+            Assert.AreEqual(Program.ConvertToColumnNumber("AA"), 27);
+            Assert.AreEqual(Program.ConvertToColumnNumber("DS"), 123);
+            Assert.AreEqual(Program.ConvertToColumnNumber("CZZ"), 2730);
+            Assert.AreEqual(Program.ConvertToColumnNumber("DAA"), 2731);
+            Assert.AreEqual(Program.ConvertToColumnNumber("FXSHRXW"), 2147483647);
+        }
+
+    }
+}
